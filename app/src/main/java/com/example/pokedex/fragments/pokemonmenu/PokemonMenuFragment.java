6a59750d312a7e8bf076f0441b4de88a010903df
MaksRoxx <maksimusroxx@gmail.com>
2024-03-19 13:36:35 +0300

    added search

--- app/src/main/java/com/example/pokedex/fragments/pokemonmenu/PokemonMenuFragment.java
+++ app/src/main/java/com/example/pokedex/fragments/pokemonmenu/PokemonMenuFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,9 +13,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.example.pokedex.R;
+import com.example.pokedex.model.Pokemon;
 import com.example.pokedex.viewmodel.PokemonViewModel;
+
+import java.util.List;
 
 public class PokemonMenuFragment extends Fragment {
     private RecyclerView rv;
@@ -36,7 +41,25 @@ public class PokemonMenuFragment extends Fragment {
         rv.setAdapter(adapter);
 
         pokemonViewModel = new ViewModelProvider(requireActivity()).get(PokemonViewModel.class);
-
         pokemonViewModel.getAllPokemon().observe(getViewLifecycleOwner(), pokemonList -> adapter.setPokemonList(pokemonList));
+
+        SearchView searchView = view.findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                pokemonViewModel.searchPokemon(newText).observe(requireActivity(), new Observer<List<Pokemon>>() {
+                    @Override
+                    public void onChanged(List<Pokemon> pokemons) {
+                        adapter.setPokemonList(pokemons);
+                    }
+                });
+                return true;
+            }
+        });
     }
 }
